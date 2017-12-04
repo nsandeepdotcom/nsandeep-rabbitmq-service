cb18ab4c90854f5ef341c502023e7ad7026c4a9c
nsandeep <nsandeepdotcom@gmail.com>
2017-12-04 17:57:35 -0600

    intial commit for nsandeep-rabbitmq-service

---
+++ src/main/java/com/nsandeep/jms/NsandeepJmsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nsandeep.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsandeepJmsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsandeepJmsServiceApplication.class, args);
+	}
+}
